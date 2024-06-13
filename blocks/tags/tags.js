2a4f85ef56b242ac6bf828c0ce729685a4297fbd
Burkhard Pauli <mail@bpauli.de>
2024-06-13 14:40:00 +0200

    feat: Add formattedTags function to remove prefix and format tags

--- blocks/tags/tags.js
+++ blocks/tags/tags.js
@@ -1,9 +1,15 @@
 import html from 'solid-js/html';
 import { render } from 'solid-js/web';
 
+function formattedTags(inputString) {
+  return inputString
+    .replace(/comwrap:products\//g, '')
+    .map((part) => part.replace('-', ' ').toUpperCase());
+}
+
 /** @param {HTMLElement} block */
 export default function decorate(block) {
-  const tag = block.textContent?.split(',')[0].trim();
+  const tag = formattedTags(block.textContent?.split(',')[0].trim());
   const blogUrl = `/${location.pathname.split('/').slice(1, 3).join('/')}?categories=${tag}`;
   const Tags = () => html`
     <a href='${blogUrl}' aria-label="tag">${tag}</a>
