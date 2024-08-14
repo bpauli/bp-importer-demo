4012f524ffea04c7639878726910ee4864fcd50e
Burkhard Pauli <mail@bpauli.de>
2024-08-14 14:06:44 +0200

    feat: Add async functionality to formattedTags function

--- blocks/tags/tags.js
+++ blocks/tags/tags.js
@@ -1,16 +1,26 @@
 import html from 'solid-js/html';
 import { render } from 'solid-js/web';
 
-function formattedTags(inputString) {
-  return inputString
-    .replace(/comwrap:products\//g, '')
-    .replace(/-/g, ' ')
-    .toUpperCase();
+
+async function formattedTags(inputString, lang) {
+  const resp = await fetch(`/taxonomy.json?sheet=${lang}`);
+  if (!resp.ok) {
+    throw new Error(`Failed to fetch data. Status: ${resp.status}`);
+  }
+
+  const json = await resp.json();
+  const tagData = json.data.find((item) => item.tag === inputString);
+
+  if (tagData) {
+    return tagData.title;
+  }
 }
 
 /** @param {HTMLElement} block */
-export default function decorate(block) {
-  const tag = formattedTags(block.textContent?.split(',')[0].trim());
+export default async function decorate(block) {
+  const langUrlSegment = document.location.pathname.split('/').at(1);
+  const lang = langUrlSegment?.match(/^(en|de)$/i)?.[0] || 'en';
+  const tag = await formattedTags(block.textContent?.split(',')[0].trim(), lang);
   const blogUrl = `/${location.pathname.split('/').slice(1, 3).join('/')}?categories=${tag}`;
   const Tags = () => html`
     <a href='${blogUrl}' aria-label="tag">${tag}</a>
